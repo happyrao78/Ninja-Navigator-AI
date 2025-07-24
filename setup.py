579a4b6eb3b5e9c95b861c9704cfab6366df3ad7
Happy Yadav <happy.hy0017@gmail.com>
2025-07-25 00:20:31 +0530

    fix:venv , requirements, and test groq & tools creation methods

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     name="Ninja Navigator AI",
     version="0.0.1",
     author="happy yadav",
-    author_email="happyrao@gmail.com",
+    author_email="happyrao7091@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements()
 )
