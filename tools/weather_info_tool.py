9cf858beebb02990e3ae51dba734d30f97fa2209
Happy Yadav <happy.hy0017@gmail.com>
2025-07-25 00:37:27 +0530

    upd:weather tool& utils

--- tools/weather_info_tool.py
+++ tools/weather_info_tool.py
@@ -1,5 +1,5 @@
 import os
-# from utils.weather_info import WeatherForecastTool
+from utils.weather_info import WeatherForecastTool
 from langchain.tools import tool
 from typing import List
 from dotenv import load_dotenv
@@ -12,10 +12,10 @@ class WeatherInfoTool:
         self.weather_tool_list = self._setup_tools()
     
     def _setup_tools(self) -> List:
-        """Setup all tools for the weather forecast tool"""
+        
         @tool
         def get_current_weather(city: str) -> str:
-            """Get current weather for a city"""
+            
             weather_data = self.weather_service.get_current_weather(city)
             if weather_data:
                 temp = weather_data.get('main', {}).get('temp', 'N/A')
@@ -25,7 +25,6 @@ class WeatherInfoTool:
         
         @tool
         def get_weather_forecast(city: str) -> str:
-            """Get weather forecast for a city"""
             forecast_data = self.weather_service.get_forecast_weather(city)
             if forecast_data and 'list' in forecast_data:
                 forecast_summary = []
